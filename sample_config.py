f0368dc7710b92307543f2b3c1d4ea94e94db83e
Tofey17 <154902093+Tofey17@users.noreply.github.com>
2024-01-27 01:19:11 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,7 +12,7 @@ class Config(object):
     TG_BOT_USERNAME = None
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/telethonArab/telethonAr1")
-    AUTONAME = os.environ.get("AUTONAME", "@iqthon")
+    AUTONAME = os.environ.get("AUTONAME", "@BDthon")
     PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID") or 0)
     PRIVATE_GROUP_ID = int(os.environ.get("PRIVATE_GROUP_ID") or 0)
     PRIVATE_CHANNEL_BOT_API_ID = int(os.environ.get("PRIVATE_CHANNEL_BOT_API_ID") or 0)
@@ -21,7 +21,7 @@ class Config(object):
     OWNER_ID = int(os.environ.get("OWNER_ID") or 0)
     PM_LOGGER_GROUP_ID = int( os.environ.get("PM_LOGGER_GROUP_ID") or os.environ.get("PM_LOGGR_BOT_API_ID") or 0 )
     PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL") or 0)
-    TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "iqthon")
+    TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "BDthon")
     THUMB_IMAGE = os.environ.get("THUMB_IMAGE", "https://telegra.ph/file/f1b9e7e1a5b0f719bc5d9.jpg")
     PORT = os.environ.get("PORT", "8080")
     NO_LOAD = [x for x in os.environ.get("NO_LOAD", "").split()]
